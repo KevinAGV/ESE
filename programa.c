bf5dcd47358aa827fb6c6efd9da839e2488eeed7
KevinAGV <kevinalberto_guerrero@ucol.mx>
2019-06-11 13:23:09 -0500

    Se creo base del archivo

---
+++ programa.c
@@ -0,0 +1,17 @@
+#include <programa.h>
+#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8)
+#include <library/stdio.h>
+void main()
+{
+   
+    printf("Bienvenido; )";
+    
+    
+   while(TRUE)
+   {
+
+
+      //TODO: User Code
+   }
+
+}
